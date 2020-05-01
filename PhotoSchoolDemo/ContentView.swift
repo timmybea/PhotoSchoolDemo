a34f4d627df2e15bd519dbe35a76115e0e7d6ad9
Tim Beals <timbeals@Tims-MacBook-Pro-2020.local>
2020-04-30 17:54:17 -0700

    -Initial project setup

---
+++ PhotoSchoolDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PhotoSchoolDemo
+//
+//  Created by Tim Beals on 4/30/20.
+//  Copyright © 2020 Tim Beals. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("How to Hold You iPhone When Taking Photos")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
